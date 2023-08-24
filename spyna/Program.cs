ea0902355a3f992ba71f8d8a107e0ed0953f6170
Svarlione <ruslan.brigockij@gmail.com>
2023-08-24 19:24:24 +0300

    Add project files.

---
+++ spyna/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Labas, kelinta valanda siandien atsikielete?");
+string userInput = Console.ReadLine();
+int userInputInteger = Convert.ToInt32(userInput);
+if (userInputInteger < 12)
+    Console.WriteLine("Geros dienos");
+else if (userInputInteger < 18)
+    Console.WriteLine("Geros popieties");
+else if (userInputInteger < 24)
+    Console.WriteLine("Gero vakoro");
